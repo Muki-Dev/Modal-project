14324a88930da0454115c81f83251fd7cd9f719f
muki Dev <mukidev881@gmail.com>
2025-01-20 23:28:59 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         welcome To React 👏
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Modal-Project
         </a>
       </header>
     </div>
